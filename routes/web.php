e3adb56a337285eab1cf9c065413ab9e0950dbe1
michaelmegally <michaelgamil8@gmail.com>
2023-02-08 17:42:15 +0200

    intial commit

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/', function () {
 
 Route::resource('products', ProductsController::class)->middleware(['auth', 'verified']);
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
