224e76ea2cafd0713bb102babd54da41b2880e51
michaelmegally <michaelgamil8@gmail.com>
2023-02-08 17:40:42 +0200

    intial commit

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::resource('products', ProductsController::class)->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+     Route::resource('products', ProductsController::class);
 });
-    Route::resource('products', ProductsController::class);
 
 require __DIR__.'/auth.php';
